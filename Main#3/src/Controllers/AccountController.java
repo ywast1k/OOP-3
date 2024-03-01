30e8561a71276d4e36cc38b0707b8e8698086941
KoNstantin_Binevskii <kbynevskij@yandex.ru>
2024-03-01 11:48:26 +0100

    *1 задание

--- Main#3/src/Controllers/AccountController.java
+++ Main#3/src/Controllers/AccountController.java
@@ -3,8 +3,13 @@ package Controllers;
 import Domen.WorkingPerson;
 // Выплата зарплаты. Информация из WPerson
 public class AccountController {
-    public  static <T extends WorkingPerson, V> void PaySalary(T person, V salary){// подключение person
+    public  static <T extends WorkingPerson, V> void PaySalary(T person, V salary){// Сортировка для безопасности
         System.out.println(person.getName() + "Выплаченная зарплата: " + salary);
 
     }
+
+public static void averageAge(){
+
+}
+
 }
--- Main#3/src/Domen/averageAge.java
+++
@@ -1,5 +0,0 @@
-package Domen;
-
-public class averageAge {
-    
-}

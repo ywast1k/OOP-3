dbdd729615f8799664d85ad23a8e051cc616e0f1
KoNstantin_Binevskii <kbynevskij@yandex.ru>
2024-03-01 16:41:13 +0100

    commit

--- Main#3/src/services/TeacherService.java
+++ Main#3/src/services/TeacherService.java
@@ -21,15 +21,15 @@ public class TeacherService implements iPersonService<Teacher> {
     public List<Teacher> getAll() {
         return this.listTeacher;
     }
-
+// Возврат 3 полей на Main Имя, возраст, степень
     @Override
     public void create(String name, int age) {
-        Teacher newTeacher = new Teacher(name, age, "Учитель");
+        Teacher newTeacher = new Teacher(name, age, name);
         this.listTeacher.add(newTeacher);
         this.count++;
         
     }
-    
+//  Сортировка
     public void SortByFIO(){
     PersonComparator<Teacher> comparator = new PersonComparator();
     this.listTeacher.sort(comparator);
